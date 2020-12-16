1d7a09ece3c6715edec3888328deb8c27a3d6ffa
Melissa Munoz <74799632+MelissaSMunoz@users.noreply.github.com>
2020-12-16 05:24:33 -0500

    Update example.py

--- example.py
+++ example.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import ADM
 
+
 #Physical properties of the star
 #-------------------------------------------------------------------------------
 #Effective temperature, Teff, is in Kelvins
@@ -17,33 +18,42 @@ Vinf = 2500.0
 Mdot = 10**(-6.0)
 Bstar = 2500.0
 
+
 #Geometric angles
 #-------------------------------------------------------------------------------
-#inclination angle, inc, in degrees
-#magnetic obliquity, beta, in degrees
+#Inclination angle, inc, in degrees
+#Magnetic obliquity, beta, in degrees
 inc = 30.
 beta = 60.
 A = inc+beta
 B = np.abs(inc-beta)
 
-#Extra paramaters
+
+#Extra parameters
 #-------------------------------------------------------------------------------
 #Smoothing length, delta
 #Vertical offset in differentlia magnitde, dm0 (constant)
 delta = 0.1 
 dm0 = 0.0
 
+
 #Calling ADM
 #-------------------------------------------------------------------------------
 phi = np.linspace(0.,1.,50) #rotational phase
-Nx = Ny = Nz = 50 #grid size 2Nx x 2Ny x 2Nz
+Nx = Ny = Nz = 50 #grid size 
 out = ADM.LC(phi, A, B, Nx, Ny, Nz, Teff, Mstar, Rstar, Vinf,  Mdot, Bstar, delta, dm0 )
+
 
 #Plotting phased light curve
 #-------------------------------------------------------------------------------
-plt.figure()
-plt.plot(phi,out)
+plt.figure(figsize=(9,6))
+plt.plot(phi,out,'k')
+plt.plot(phi+1,out,'k')
+plt.plot(phi-1,out,'k')
 plt.gca().invert_yaxis()
+plt.xlabel('Rotational phase',fontsize=14)
+plt.ylabel('Differential magnitude [mag]',fontsize=14)
+plt.xlim([-0.5,1.5])
 plt.show()
 
 
